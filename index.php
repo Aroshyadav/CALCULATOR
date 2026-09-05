e236402eb2400997b700a830fc7e0fb47ed2988f
Aroshyadav <deviprasad63927@gmail.com>
2026-09-05 15:14:22 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<DOCUMENT! html>
+</html>
+<head>
+        <title>BASIC CALCULATOR</title>
+</head>
+<body style="background-color:lightgreen;boder:5px">
+        <h1 style="color:blue;">Do Arithmatic With Your Brain Coins<h1>
+        <h3 style="color:red;">Applicable operations are: <br>
+        <ul>
+        <list>Addition</list>
+        <list>Substraction</list>
+        <list>Division</list>
+        <list>Multiplication</list>
+        <ul>
+        <h3>
+<div>
+        <form action="process.php" method="POST" pstyle="border:3px;color:violet;">
+<section>
+<label>Enter your first coin</label>
+<input type="number" name="number1">;
+</section>
+<section>
+<label>Enter the operand</label>
+<input type="string" name="operand">
+</section>
+<section>
+<label>Enter your second coin</label>
+<input type="number" name="number2">
+</section>
+<section>
+<button type="submit">GET THE RESULT<button>
+<section>
+</form>
+</div>
+</body>
+</html>
